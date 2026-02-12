45c96fb0d9b8d0b7d63cefffcf6ac216b69f421f
VansFannel <liberado27@hotmail.com>
2026-02-12 13:04:14 +0100

    Done during the fourth class.

---
+++ Assets/Kits/InventorySystem/Scripts/InventoryItemUI.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryItemUI : MonoBehaviour
+{
+    [SerializeField] InventoryItemDefinition inventoryItemDefinition;
+
+    Image image;
+    TextMeshProUGUI text;
+    Button[] buttons;
+
+    enum ButtonAction
+    {
+        Discard,
+        Use,
+        Give,
+        Sell
+    }
+
+    private void Awake()
+    {
+        buttons = GetComponentsInChildren<Button>();
+        image = GetComponentInChildren<Image>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        buttons[(int)ButtonAction.Discard].onClick.AddListener(OnDiscard);
+        buttons[(int)ButtonAction.Use].onClick.AddListener(OnUse);
+        buttons[(int)ButtonAction.Give].onClick.AddListener(OnGive);
+        buttons[(int)ButtonAction.Sell].onClick.AddListener(OnSell);
+    }
+
+    private void OnDisable()
+    {
+        buttons[(int)ButtonAction.Discard].onClick.RemoveListener(OnDiscard);
+        buttons[(int)ButtonAction.Use].onClick.RemoveListener(OnUse);
+        buttons[(int)ButtonAction.Give].onClick.RemoveListener(OnGive);
+        buttons[(int)ButtonAction.Sell].onClick.RemoveListener(OnSell);
+    }
+
+    private void Start()
+    {
+        Init(inventoryItemDefinition);
+    }
+
+    public void Init(InventoryItemDefinition definition)
+    {
+        text.text = definition.itemName;
+        image.sprite = definition.sprite;
+    }
+
+    void OnDiscard()
+    {
+        Debug.Log("OnDiscard", this);
+    }
+
+    void OnUse()
+    {
+        Debug.Log("OnUse", this);
+
+    }
+
+    void OnSell()
+    {
+        Debug.Log("OnSell", this);
+    }
+
+    void OnGive()
+    {
+        Debug.Log("OnGive", this);
+    }
+}
